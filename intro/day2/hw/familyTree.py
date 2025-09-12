c33f3e21fd50d35d13efc4fe157b0c33b9531c89
David Richman <david.c.richman@gmail.com>
2025-09-12 01:01:18 -0400

    renamed homework folder to hw

---
+++ intro/day2/hw/familyTree.py
@@ -0,0 +1,5 @@
+#TODO Use objects to create a family tree
+#Each person should have a name and an age
+#Each person should be able to have children (other people)
+#Create functions to add children to a person
+#Finally, create a few people and print out their family tree
