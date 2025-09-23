632bcd1529fe5dab5e50beec95b08a97fffee168
David Richman <david.c.richman@gmail.com>
2025-09-23 16:27:49 -0400

    Co-authored-by: victoriasant233-glitch <victoriasant233-glitch@users.noreply.github.com>

--- flask1/app.py
+++ flask1/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 from markupsafe import escape
+import os
+local_dir = os.path.dirname(__file__)
 
 app=Flask(__name__)
 
@@ -9,14 +11,28 @@ def hello_world():
 
 @app.route("/info")
 def info():
-    return "<h1> All About Me </h1>" + "<p> Favorite color is purple! </p>"
-
-@app.route("/wrong/<game>")
-def wrong(game):
-    return render_template("index.html", game=escape(game))
+    about_me = [
+        "Favorite color is Purple",
+        "Favorite band is ACDC"
+    ]
+    return render_template("info.html")
 
 @app.route("/form", methods=["GET","POST"])
 def form():
+    context = {
+        'success': False
+    }
     if request.method=="POST":
+        # request.form -> {name, last}
+        #printing first name
         app.logger.info(request.form.get("name"))
-    return render_template("form.html")
+        first=request.form.get("name")
+        last=request.form.get("last")
+        color=request.form.get("color")
+        band=request.form.get("band")
+        food=request.form.get("food")
+        f = open(local_dir + "/formdata.csv", "a")
+        f.write(f"{last}, {first}, {color}, {band}, {food}\n")
+        context["success"] = True
+    
+    return render_template("form.html", **context)
